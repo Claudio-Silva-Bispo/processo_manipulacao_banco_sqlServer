5127f683d15d8739e418d3c2716a51cb0993ef1d
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2024-09-12 13:33:53 -0300

    Update funcao_conectar.py

--- funcao_conectar.py
+++ funcao_conectar.py
@@ -4,9 +4,9 @@ import pymssql
 
 def conectar_sql():
     server = 'tms-drp-oficial.database.windows.net'
-    database = 'tms'
-    user = 'appTableau'
-    password = 'Tjd82#jks2'
+    database = ''
+    user = ''
+    password = ''
 
     conn = pymssql.connect(server=server, database=database, user=user, password=password)
 
